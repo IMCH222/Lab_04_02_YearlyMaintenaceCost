80daba8e5735fa4c05b8a0758fcb00c7994e6e69
IMCH222 <chukwuim@mail.uc.edu>
2023-06-04 11:44:10 -0400

    Finished task 2

--- src/YearlyMaintenanceCost.java
+++ src/YearlyMaintenanceCost.java
@@ -1,5 +1,13 @@
-public class YearlyMaintenanceCost {
+public class YearlyMaintenanceCost
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    double springCost = 150.25;
+    double summerCost = 250.25;
+    double autumnCost = 125.25;
+    double winterCost = 175.25;
+    double yearlyCost = 0;
+    yearlyCost = springCost + summerCost + autumnCost + winterCost;
+        System.out.println("The cost of maintenance for the year is:" + " " + "$" + yearlyCost);
+
     }
 }
